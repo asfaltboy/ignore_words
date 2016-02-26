9c4daad3661dcc8efe47fb21a80b4c22aa6cf676
Pavel Savchenko <asfaltboy@gmail.com>
2016-02-26 14:38:54 +0100

    Do the actual work in an async call
    a futile attempt at resolving issue #3

--- ignoreword.py
+++ ignoreword.py
@@ -1,12 +1,16 @@
-import sublime, sublime_plugin
+import sublime
+import sublime_plugin
+
 
 class Ignoreword(sublime_plugin.EventListener):
-  def on_text_command(self, view, command_name, args):
-    if command_name == 'ignore_word' :
-      settings = sublime.load_settings('Preferences.sublime-settings')
-      ignored_words = settings.get('ignored_words', [])
-      word = args['word']
-      if word and word not in ignored_words :
-        ignored_words.append(word)
-        settings.set('ignored_words', ignored_words)
-        sublime.save_settings('Preferences.sublime-settings')
+    def add_ignore_word(self, word):
+        settings = sublime.load_settings('Preferences.sublime-settings')
+        ignored_words = settings.get('ignored_words', [])
+        if word and word not in ignored_words:
+            ignored_words.append(word)
+            settings.set('ignored_words', ignored_words)
+            sublime.save_settings('Preferences.sublime-settings')
+
+    def on_text_command(self, view, command_name, args):
+        if command_name == 'ignore_word':
+            sublime.set_timeout_async(lambda: self.add_ignore_word(args['word']))
